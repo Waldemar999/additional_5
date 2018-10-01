870702307aaa83770da94302cc789482f3c16e8a
Waldemar999 <vlad.voropaev0q@gmail.com>
2018-10-01 13:48:16 +0300

    Task 6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketacketsConfig) {
+  var stack = [];
+    for (let bracket of str) {
+        const couple = bracketacketsConfig.find(b => b.includes(bracket));
+        if(bracket === couple[1] && couple[0] === stack[stack.length - 1]) {
+          stack.pop()
+          } 
+        else { 
+          stack.push(bracket);
+        }
+    }
+    return !stack.length; 
+} 
